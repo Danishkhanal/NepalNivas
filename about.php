8185a9e87708818412ce2323d2119357b92ffdb8
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-02-13 21:31:16 +0545

    Room and customer

---
+++ about.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link  rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+  <?php require('inc/links.php'); ?>
+  <title><?php echo $settings_r['site_title'] ?> - ABOUT</title>
+  <style>
+    .box{
+      border-top-color: var(--teal) !important;
+    }
+  </style>
+</head>
+<body class="bg-light">
+
+  <?php require('inc/header.php'); ?>
+
+  <div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">ABOUT US</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+    NepalNivas is your ultimate hotel booking platform, offering a wide range of accommodations across Nepal.
+    Find affordable, <br>comfortable stays with easy booking, reliable service, and seamless experiences for all travelers.
+    </p>
+  </div>
+
+  <div class="container">
+    <div class="row justify-content-between align-items-center">
+      <div class="col-lg-6 col-md-5 mb-4 order-lg-1 order-md-1 order-2">
+        <h3 class="mb-3">Chairman</h3>
+        <p>
+        Danish Khanal, the Chairman of NepalNivas, brings expertise and vision to the hospitality industry. 
+        With a focus on excellence, he drives innovation, ensuring exceptional experiences and service for travelers across Nepal.
+        </p>
+      </div>
+      <div class="col-lg-5 col-md-5 mb-4 order-lg-2 order-md-2 order-1">
+      <img src="image/User.jpg" class="w-100" style="height: 20;">
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 mb-4 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+          <img src="images/about/hotel.svg" width="70px">
+          <h4 class="mt-3">100+ ROOMS</h4>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+          <img src="images/about/customers.svg" width="70px">
+          <h4 class="mt-3">200+ CUSTOMERS</h4>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+          <img src="images/about/rating.svg" width="70px">
+          <h4 class="mt-3">150+ REVIEWS</h4>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4 px-4">
+        <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
+          <img src="images/about/staff.svg" width="70px">
+          <h4 class="mt-3">200+ STAFFS</h4>
+        </div>
+      </div>
+    </div>
+  </div>
