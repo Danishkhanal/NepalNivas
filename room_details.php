954e262c2216690944bc6272b04429dd3eae19a5
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-02-17 16:03:54 +0545

    room detail

--- room_details.php
+++ room_details.php
@@ -26,3 +26,58 @@
 
     $room_data = mysqli_fetch_assoc($room_res);
   ?>
+  <div class="container">
+    <div class="row">
+
+      <div class="col-12 my-5 mb-4 px-4">
+        <h2 class="fw-bold"><?php echo $room_data['name'] ?></h2>
+        <div style="font-size: 14px;">
+          <a href="index.php" class="text-secondary text-decoration-none">HOME</a>
+          <span class="text-secondary"> > </span>
+          <a href="rooms.php" class="text-secondary text-decoration-none">ROOMS</a>
+        </div>
+      </div>
+
+      <div class="col-lg-7 col-md-12 px-4">
+        <div id="roomCarousel" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <?php 
+
+              $room_img = ROOMS_IMG_PATH."thumbnail.jpg";
+              $img_q = mysqli_query($con,"SELECT * FROM `room_images` 
+                WHERE `room_id`='$room_data[id]'");
+
+              if(mysqli_num_rows($img_q)>0)
+              {
+                $active_class = 'active';
+
+                while($img_res = mysqli_fetch_assoc($img_q))
+                {
+                  echo"
+                    <div class='carousel-item $active_class'>
+                      <img src='".ROOMS_IMG_PATH.$img_res['image']."' class='d-block w-100 rounded'>
+                    </div>
+                  ";
+                  $active_class='';
+                }
+
+              }
+              else{
+                echo"<div class='carousel-item active'>
+                  <img src='$room_img' class='d-block w-100'>
+                </div>";
+              }
+
+            ?>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#roomCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#roomCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+
+      </div>
