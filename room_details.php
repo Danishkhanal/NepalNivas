656d378b91b9394f41f82a26ecdc9a752910c7eb
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-02-17 16:03:02 +0545

    room details

---
+++ room_details.php
@@ -0,0 +1,28 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php require('inc/links.php'); ?>
+  <title><?php echo $settings_r['site_title'] ?> - ROOM DETAILS</title>
+</head>
+<body class="bg-light">
+
+  <?php require('inc/header.php'); ?>
+
+  <?php 
+    if(!isset($_GET['id'])){
+      redirect('rooms.php');
+    }
+
+    $data = filteration($_GET);
+
+    $room_res = select("SELECT * FROM `rooms` WHERE `id`=? AND `status`=? AND `removed`=?",[$data['id'],1,0],'iii');
+
+    if(mysqli_num_rows($room_res)==0){
+      redirect('rooms.php');
+    }
+
+    $room_data = mysqli_fetch_assoc($room_res);
+  ?>
