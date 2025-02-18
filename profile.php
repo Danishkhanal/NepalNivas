9362a7161a365af36e1c9aaf2049a00f93c1d5f8
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-02-18 22:02:07 +0545

    profile

--- profile.php
+++ profile.php
@@ -24,3 +24,14 @@
 
     $u_fetch = mysqli_fetch_assoc($u_exist);
   ?>
+  <div class="container">
+    <div class="row">
+
+      <div class="col-12 my-5 px-4">
+        <h2 class="fw-bold">PROFILE</h2>
+        <div style="font-size: 14px;">
+          <a href="index.php" class="text-secondary text-decoration-none">HOME</a>
+          <span class="text-secondary"> > </span>
+          <a href="#" class="text-secondary text-decoration-none">PROFILE</a>
+        </div>
+      </div>
