872b2f4e6129ecaadc51aa7d89095183eab1a4c4
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-02-18 22:02:30 +0545

    profile

--- profile.php
+++ profile.php
@@ -35,3 +35,33 @@
           <a href="#" class="text-secondary text-decoration-none">PROFILE</a>
         </div>
       </div>
+      <div class="col-12 mb-5 px-4">
+        <div class="bg-white p-3 p-md-4 rounded shadow-sm">
+          <form id="info-form">
+            <h5 class="mb-3 fw-bold">Basic Information</h5>
+            <div class="row">
+              <div class="col-md-4 mb-3">
+                <label class="form-label">Name</label>
+                <input name="name" type="text" value="<?php echo $u_fetch['name'] ?>" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label class="form-label">Phone Number</label>
+                <input name="phonenum" type="number" value="<?php echo $u_fetch['phonenum'] ?>" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label class="form-label">Date of birth</label>
+                <input name="dob" type="date" value="<?php echo $u_fetch['dob'] ?>" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-4 mb-3">
+                <label class="form-label">Pincode</label>
+                <input name="pincode" type="number" value="<?php echo $u_fetch['pincode'] ?>" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-8 mb-4">
+                <label class="form-label">Address</label>
+                <textarea name="address" class="form-control shadow-none" rows="1" required><?php echo $u_fetch['address'] ?></textarea>
+              </div>
+            </div>
+            <button type="submit" class="btn text-white custom-bg shadow-none">Save Changes</button>
+          </form>
+        </div>
+      </div>
