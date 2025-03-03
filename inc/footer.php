b66f84c7106eaed7fbba6d877934614f523ce18a
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-03-03 19:16:33 +0545

    foot

---
+++ inc/footer.php
@@ -0,0 +1,16 @@
+<div class="container-fluid bg-white mt-5">
+  <div class="row">
+    <div class="col-lg-4 p-4">
+      <h3 class="h-font fw-bold fs-3 mb-2"><?php echo $settings_r['site_title'] ?></h3>
+      <p>
+        <?php echo $settings_r['site_about'] ?>
+      </p>
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Links</h5>
+      <a href="index.php" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a> <br>
+      <a href="rooms.php" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a> <br>
+      <a href="facilities.php" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a> <br>
+      <a href="contact.php" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a> <br>
+      <a href="about.php" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+    </div>
