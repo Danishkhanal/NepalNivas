48a8e1da3bc4c08cbb56981ac9a0e317aeb22a60
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-03-16 21:37:24 +0545

    header

--- inc/header.php
+++ inc/header.php
@@ -87,3 +87,114 @@
     </div>
   </div>
 </div>
+<div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <form id="register-form">
+        <div class="modal-header">
+          <h5 class="modal-title d-flex align-items-center">
+            <i class="bi bi-person-lines-fill fs-3 me-2"></i> User Registration
+          </h5>
+          <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-6 mb-3">
+                <label class="form-label">Name</label>
+                <input name="name" type="text" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label class="form-label">Email</label>
+                <input name="email" type="email" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label class="form-label">Phone Number</label>
+                <input name="phonenum" type="number" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label class="form-label">Picture</label>
+                <input name="profile" type="file" accept=".jpg, .jpeg, .png, .webp" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-12 mb-3">
+                <label class="form-label">Address</label>
+                <textarea name="address" class="form-control shadow-none" rows="1" required></textarea>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label class="form-label">Pincode</label>
+                <input name="pincode" type="number" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label class="form-label">Date of birth</label>
+                <input name="dob" type="date" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label class="form-label">Password</label>
+                <input name="pass" type="password" class="form-control shadow-none" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label class="form-label">Confirm Password</label>
+                <input name="cpass" type="password" class="form-control shadow-none" required>
+              </div>
+            </div>
+          </div>
+          <div class="text-center my-1">
+            <button type="submit" class="btn btn-dark shadow-none">REGISTER</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="forgotModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form id="forgot-form">
+        <div class="modal-header">
+          <h5 class="modal-title d-flex align-items-center">
+            <i class="bi bi-person-circle fs-3 me-2"></i> Forgot Password
+          </h5>
+        </div>
+        <div class="modal-body">
+          <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+            Note: A link will be sent to your email to reset your password!
+          </span>
+          <div class="mb-4">
+            <label class="form-label">Email</label>
+            <input type="email" name="email" required class="form-control shadow-none">
+          </div>
+          <div class="mb-2 text-end">
+            <button type="button" class="btn shadow-none p-0 me-2" data-bs-toggle="modal" data-bs-target="#loginModal" data-bs-dismiss="modal">
+              CANCEL
+            </button>
+            <button type="submit" class="btn btn-dark shadow-none">SEND LINK</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="compareRoomsModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title d-flex align-items-center">
+          <i class="bi bi-arrow-left-right fs-3 me-2"></i> Compare Rooms
+        </h5>
+        <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="container-fluid" id="compare-rooms-list">
+          <div class="row" id="room-selection-area"></div>
+          <div class="row mt-3" id="comparison-area" style="display:none;"></div>
+        </div>
+        <p id="compare-rooms-error" class="text-danger" style="display:none;">** Select at least one and maximum two rooms to compare! **</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary shadow-none" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" id="compare-rooms-btn" class="btn btn-dark shadow-none" onclick="compareSelectedRooms()" disabled>Compare Selected Rooms</button>
+      </div>
+    </div>
+  </div>
+</div>
