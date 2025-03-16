b359058ab07d43ddd7af6a19ebacf37bc1e95c68
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-03-16 21:34:21 +0545

    header

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<nav id="nav-bar" class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php"><?php echo $settings_r['site_title'] ?></a>
+    <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link me-2" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="rooms.php">Rooms</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="facilities.php">Facilities</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="contact.php">Contact us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+    <button type="button" class="nav-link" data-bs-toggle="modal" data-bs-target="#compareRoomsModal" style="border: none; background: transparent;">Compare Room</button>
+</li>
