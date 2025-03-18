efaae76cc305e93326c5bf5399ec231e67d03492
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-03-18 19:48:42 +0545

    pdf

--- generate_pdf.php
+++ generate_pdf.php
@@ -3,3 +3,24 @@
   require('inc/essentials.php');
   require('inc/db_config.php');
   require('inc/mpdf/vendor/autoload.php');
+  adminLogin();
+
+  if(isset($_GET['gen_pdf']) && isset($_GET['id']))
+  {
+    $frm_data = filteration($_GET);
+
+    $query = "SELECT bo.*, bd.*,uc.email FROM `booking_order` bo
+      INNER JOIN `booking_details` bd ON bo.booking_id = bd.booking_id
+      INNER JOIN `user_cred` uc ON bo.user_id = uc.id
+      WHERE ((bo.booking_status='booked' AND bo.arrival=1) 
+      OR (bo.booking_status='cancelled' AND bo.refund=1)
+      OR (bo.booking_status='payment failed')) 
+      AND bo.booking_id = '$frm_data[id]'";
+
+    $res = mysqli_query($con,$query);
+    $total_rows = mysqli_num_rows($res);
+
+    if($total_rows==0){
+      header('location: dashboard.php');
+      exit;
+    }
