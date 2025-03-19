de1ea4e6ccabf15e1d39bb1dc79938ac1c4a110a
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-03-19 21:49:21 +0545

    booking admin part

--- admin/ajax/booking_records.php
+++ admin/ajax/booking_records.php
@@ -4,3 +4,32 @@
   require('../inc/essentials.php');
   date_default_timezone_set("Asia/Kolkata");
   adminLogin();
+  
+  if(isset($_POST['get_bookings']))
+  {
+    $frm_data = filteration($_POST);
+
+    $limit = 2;
+    $page = $frm_data['page'];
+    $start = ($page-1) * $limit;
+
+    $query = "SELECT bo.*, bd.* FROM `booking_order` bo
+      INNER JOIN `booking_details` bd ON bo.booking_id = bd.booking_id
+      WHERE ((bo.booking_status='booked' AND bo.arrival=1) 
+      OR (bo.booking_status='cancelled' AND bo.refund=1)
+      OR (bo.booking_status='payment failed')) 
+      AND (bo.order_id LIKE ? OR bd.phonenum LIKE ? OR bd.user_name LIKE ?) 
+      ORDER BY bo.booking_id DESC";
+
+    $res = select($query,["%$frm_data[search]%","%$frm_data[search]%","%$frm_data[search]%"],'sss');
+    
+    $limit_query = $query ." LIMIT $start,$limit";
+    $limit_res = select($limit_query,["%$frm_data[search]%","%$frm_data[search]%","%$frm_data[search]%"],'sss');
+
+    $total_rows = mysqli_num_rows($res);
+
+    if($total_rows==0){
+      $output = json_encode(["table_data"=>"<b>No Data Found!</b>", "pagination"=>'']);
+      echo $output;
+      exit;
+    }
