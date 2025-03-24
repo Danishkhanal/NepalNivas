455622587c41bcbdeec12ad559cb73f89163497d
Danish khanal <np03cs4s230134@heraldcollege.edu.np>
2025-03-24 21:41:42 +0545

    email part

--- email_confirm.php
+++ email_confirm.php
@@ -5,3 +5,15 @@
 
   if(isset($_GET['email_confirmation']))
   {
+    $data = filteration($_GET);
+
+    $query = select("SELECT * FROM `user_cred` WHERE `email`=? AND `token`=? LIMIT 1",
+      [$data['email'],$data['token']],'ss');
+
+    if(mysqli_num_rows($query)==1)
+    {
+      $fetch = mysqli_fetch_assoc($query);
+
+      if($fetch['is_verified']==1){
+        echo"<script>alert('Email already verified!')</script>";
+      }
